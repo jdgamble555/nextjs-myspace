1e03a2f7de6be41b8f4d1c8538497692607f6bd8
Jonathan Gamble <jdgamble555@gmail.com>
2023-06-11 08:41:07 -0500

    complete

---
+++ app/users/[id]/page.tsx
@@ -0,0 +1,38 @@
+import { prisma } from '@/app/lib/prisma';
+//import FollowButton from '@/components/FollowButton/FollowButton';
+
+import { Metadata } from 'next';
+import Image from 'next/image';
+
+interface Props {
+  params: {
+    id: string;
+  };
+}
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const user = await prisma.user.findUnique({ where: { id: params.id } });
+  return { title: `User profile of ${user?.name}` };
+}
+
+export default async function UserProfile({ params }: Props) {
+  const user = await prisma.user.findUnique({ where: { id: params.id } });
+  const { name, bio, image, id } = user ?? {};
+
+  return (
+    <div>
+      <h1>{name}</h1>
+
+      <Image
+        width={300}
+        src={image ?? '/mememan.webp'}
+        alt={`${name}'s profile`}
+        height={300}
+      />
+
+      <h3>Bio</h3>
+      <p>{bio}</p>
+
+    </div>
+  );
+}
